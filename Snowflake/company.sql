3f82dddd328d03e1934d092e92660b63950afff4
atifrani <52487696+atifrani@users.noreply.github.com>
2025-04-04 15:10:36 +0200

    Update company.sql

--- Snowflake/company.sql
+++ Snowflake/company.sql
@@ -1,7 +1,6 @@
 -- create retail-db database
 create database retaildb;
 
-
 -- create table categories:
 create table retaildb.public.categories
 (
@@ -65,25 +64,56 @@ create stage company_csv URL ='s3://logbrain-datalake/datasets/company';
 list @company_csv;
 
 -- Create file format
-CREATE or replace FILE FORMAT csv_with_header 
+CREATE or replace FILE FORMAT tsv_with_header 
 TYPE = 'CSV' 
 FIELD_DELIMITER = '\t' 
 RECORD_DELIMITER = '\n' 
 SKIP_HEADER = 1
 null_if = ('');
 
+CREATE or replace FILE FORMAT csv_with_header 
+TYPE = 'CSV' 
+FIELD_DELIMITER = ',' 
+RECORD_DELIMITER = '\n' 
+SKIP_HEADER = 1
+null_if = ('');
+
 CREATE or replace FILE FORMAT csv_without_header 
 TYPE = 'CSV' 
-FIELD_DELIMITER = '\t' 
+FIELD_DELIMITER = ',' 
 RECORD_DELIMITER = '\n' 
 SKIP_HEADER = 0
 field_optionally_enclosed_by = '\042'
 null_if = ('')
 ;
 
--- load data into customers table
-copy into customers  from @company_csv file_format=csv_with_header PATTERN = '.*customers.*' ;
-
+-- load data into customers 
+copy into customers  from @company_csv file_format=tsv_with_header PATTERN = '.*customers.*' ;
 select * from customers;
 
 
+-- load data into categories
+copy into categories  from @company_csv file_format=csv_with_header PATTERN = '.*categories.*' ;
+select * from categories;
+
+-- load data into departments
+copy into departments  from @company_csv file_format=csv_with_header PATTERN = '.*departments.*' ;
+select * from departments;
+
+-- load data into products
+copy into products  from @company_csv file_format=csv_without_header PATTERN = '.*products.*' ;
+select * from products;
+
+-- load data into order_items
+copy into order_items  from @company_csv file_format=csv_without_header PATTERN = '.*order_items.*' ;
+select * from order_items;
+
+-- load data into orders
+copy into orders  from @company_csv file_format=csv_without_header PATTERN = '.*orders.*' ;
+select * from orders;
+
+
+Now, let's create a new tables/datamarts:
+
+Create table top_product (product_id, product_name, category_name, month, year, sales )
+Create table top_customer (customer_id, customer_name, customer_street, customer_city,customer_zipcode, month, year, sales )
